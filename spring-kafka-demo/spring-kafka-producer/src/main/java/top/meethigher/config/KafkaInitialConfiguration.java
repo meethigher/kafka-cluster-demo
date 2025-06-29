4a2ddafcab11aec2f11077af25a15cd708541cb0
meethigher <meethigher@qq.com>
2025-06-29 10:42:18 +0800

    feat: 添加注释，记录springkafka底层对topic初始化的实现

--- spring-kafka-demo/spring-kafka-producer/src/main/java/top/meethigher/config/KafkaInitialConfiguration.java
+++ spring-kafka-demo/spring-kafka-producer/src/main/java/top/meethigher/config/KafkaInitialConfiguration.java
@@ -17,7 +17,8 @@ public class KafkaInitialConfiguration {
         /**
          * 实际使用时，分区数应大于kafka broker数
          * 备份数与kafka broker数一致即可
-         * 不论消费者或是生产者，都可以初始化topic。但要注意服务端开启自动创建auto.create.topics.enable=true，默认就为true
+         * 不论消费者或是生产者，都可以初始化topic，参考org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration#kafkaAdmin()，本质上都依赖于org.apache.kafka.clients.admin.AdminClient
+         * 但要注意服务端开启自动创建auto.create.topics.enable=true，默认就为true
          */
         return new NewTopic(topic1, 3, (short) 3);
     }
